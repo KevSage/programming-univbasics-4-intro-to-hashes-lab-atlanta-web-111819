8f2c736de703e39f919021b1d228d9db1f200837
Kevin Sage <ksagedeveloper@gmail.com>
2019-11-05 19:22:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,15 +21,12 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  new_hash = {
-    key => value
+  key[value]
   }
 end
 
 def read_from_hash(hash, key)
-  if hash[key] !== nil
-    hash[key] = key
-  end
+  
 end
 
 def update_counting_hash(hash, key)
